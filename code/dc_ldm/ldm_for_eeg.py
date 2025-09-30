1e7b7568a8c6e86d34b8a1a99cd514c6e4b40fd3
bbaaii <40818542+bbaaii@users.noreply.github.com>
2025-09-30 13:12:55 -0500

    Update ldm_for_eeg.py

--- code/dc_ldm/ldm_for_eeg.py
+++ code/dc_ldm/ldm_for_eeg.py
@@ -193,7 +193,6 @@ class eLDM:
                 if limit is not None:
                     if count >= limit:
                         break
-                print(item)
                 latent = item['eeg']
                 gt_image = rearrange(item['image'], 'h w c -> 1 c h w') # h w c
                 print(f"rendering {num_samples} examples in {ddim_steps} steps.")
