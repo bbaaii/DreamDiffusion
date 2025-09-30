93dd78c1ec282b8ab347523fb9ff2a5e7c3ee63d
bbaaii <40818542+bbaaii@users.noreply.github.com>
2025-09-30 14:57:52 -0500

    Update ldm_for_eeg.py

--- code/dc_ldm/ldm_for_eeg.py
+++ code/dc_ldm/ldm_for_eeg.py
@@ -228,3 +228,141 @@ class eLDM:
         model = model.to('cpu')
         
         return grid, (255. * torch.stack(all_samples, 0).cpu().numpy()).astype(np.uint8)
+
+
+
+
+class eLDM_eval:
+
+    def __init__(self, config_path, num_voxels, device=torch.device('cpu'),
+                 pretrain_root='../pretrains/',
+                 logger=None, ddim_steps=250, global_pool=True, use_time_cond=False, clip_tune = True, cls_tune = False):
+        self.config_path = config_path # 
+        config = OmegaConf.load(self.config_path)
+        config.model.params.unet_config.params.use_time_cond = use_time_cond
+        config.model.params.unet_config.params.global_pool = global_pool
+
+        self.cond_dim = config.model.params.unet_config.params.context_dim
+
+        model = instantiate_from_config(config.model)
+
+        model.cond_stage_trainable = True
+        model.cond_stage_model = cond_stage_model(None, num_voxels, self.cond_dim, global_pool=global_pool, clip_tune = clip_tune,cls_tune = cls_tune)
+
+        model.ddim_steps = ddim_steps
+        model.re_init_ema()
+        if logger is not None:
+            logger.watch(model, log="all", log_graph=False)
+
+        model.p_channels = config.model.params.channels
+        model.p_image_size = config.model.params.image_size
+        model.ch_mult = config.model.params.first_stage_config.params.ddconfig.ch_mult
+
+        
+        self.device = device    
+        self.model = model
+        
+        self.model.clip_tune = clip_tune
+        self.model.cls_tune = cls_tune
+
+        self.ldm_config = config
+        self.pretrain_root = pretrain_root
+        self.fmri_latent_dim = model.cond_stage_model.fmri_latent_dim
+
+    def finetune(self, trainers, dataset, test_dataset, bs1, lr1,
+                output_path, config=None):
+        config.trainer = None
+        config.logger = None
+        self.model.main_config = config
+        self.model.output_path = output_path
+        # self.model.train_dataset = dataset
+        self.model.run_full_validation_threshold = 0.15
+        # stage one: train the cond encoder with the pretrained one
+      
+        # # stage one: only optimize conditional encoders
+        print('\n##### Stage One: only optimize conditional encoders #####')
+        dataloader = DataLoader(dataset, batch_size=bs1, shuffle=True)
+        test_loader = DataLoader(test_dataset, batch_size=bs1, shuffle=False)
+        self.model.unfreeze_whole_model()
+        self.model.freeze_first_stage()
+        # self.model.freeze_whole_model()
+        # self.model.unfreeze_cond_stage()
+
+        self.model.learning_rate = lr1
+        self.model.train_cond_stage_only = True
+        self.model.eval_avg = config.eval_avg
+        trainers.fit(self.model, dataloader, val_dataloaders=test_loader)
+
+        self.model.unfreeze_whole_model()
+        
+        torch.save(
+            {
+                'model_state_dict': self.model.state_dict(),
+                'config': config,
+                'state': torch.random.get_rng_state()
+
+            },
+            os.path.join(output_path, 'checkpoint.pth')
+        )
+        
+
+    @torch.no_grad()
+    def generate(self, fmri_embedding, num_samples, ddim_steps, HW=None, limit=None, state=None, output_path = None):
+        # fmri_embedding: n, seq_len, embed_dim
+        all_samples = []
+        if HW is None:
+            shape = (self.ldm_config.model.params.channels, 
+                self.ldm_config.model.params.image_size, self.ldm_config.model.params.image_size)
+        else:
+            num_resolutions = len(self.ldm_config.model.params.first_stage_config.params.ddconfig.ch_mult)
+            shape = (self.ldm_config.model.params.channels,
+                HW[0] // 2**(num_resolutions-1), HW[1] // 2**(num_resolutions-1))
+
+        model = self.model.to(self.device)
+        sampler = PLMSSampler(model)
+        # sampler = DDIMSampler(model)
+        if state is not None:
+            torch.cuda.set_rng_state(state)
+            
+        with model.ema_scope():
+            model.eval()
+            for count, item in enumerate(fmri_embedding):
+                if limit is not None:
+                    if count >= limit:
+                        break
+                # print(item)
+                latent = item['eeg']
+                gt_image = rearrange(item['image'], 'h w c -> 1 c h w') # h w c
+                print(f"rendering {num_samples} examples in {ddim_steps} steps.")
+                # assert latent.shape[-1] == self.fmri_latent_dim, 'dim error'
+                
+                c, re_latent = model.get_learned_conditioning(repeat(latent, 'h w -> c h w', c=num_samples).to(self.device))
+                # c = model.get_learned_conditioning(repeat(latent, 'h w -> c h w', c=num_samples).to(self.device))
+                samples_ddim, _ = sampler.sample(S=ddim_steps, 
+                                                conditioning=c,
+                                                batch_size=num_samples,
+                                                shape=shape,
+                                                verbose=False)
+
+                x_samples_ddim = model.decode_first_stage(samples_ddim)
+                x_samples_ddim = torch.clamp((x_samples_ddim+1.0)/2.0, min=0.0, max=1.0)
+                gt_image = torch.clamp((gt_image+1.0)/2.0, min=0.0, max=1.0)
+                
+                all_samples.append(torch.cat([gt_image, x_samples_ddim.detach().cpu()], dim=0)) # put groundtruth at first
+                if output_path is not None:
+                    samples_t = (255. * torch.cat([gt_image, x_samples_ddim.detach().cpu()], dim=0).numpy()).astype(np.uint8)
+                    for copy_idx, img_t in enumerate(samples_t):
+                        img_t = rearrange(img_t, 'c h w -> h w c')
+                        Image.fromarray(img_t).save(os.path.join(output_path, 
+                            f'./test{count}-{copy_idx}.png'))
+        
+        # display as grid
+        grid = torch.stack(all_samples, 0)
+        grid = rearrange(grid, 'n b c h w -> (n b) c h w')
+        grid = make_grid(grid, nrow=num_samples+1)
+
+        # to image
+        grid = 255. * rearrange(grid, 'c h w -> h w c').cpu().numpy()
+        model = model.to('cpu')
+        
+        return grid, (255. * torch.stack(all_samples, 0).cpu().numpy()).astype(np.uint8)
